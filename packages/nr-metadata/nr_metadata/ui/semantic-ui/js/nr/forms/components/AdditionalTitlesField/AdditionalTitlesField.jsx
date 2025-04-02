efe5b4dcf3ae093040994a7c9c76fe5512ae8dc9
ducica <dusanstojanovic256@gmail.com>
2025-04-02 15:29:52 +0200

    fixed additional titles field

--- packages/nr-metadata/nr_metadata/ui/semantic-ui/js/nr/forms/components/AdditionalTitlesField/AdditionalTitlesField.jsx
+++ packages/nr-metadata/nr_metadata/ui/semantic-ui/js/nr/forms/components/AdditionalTitlesField/AdditionalTitlesField.jsx
@@ -39,6 +39,11 @@ export const AdditionalTitlesField = ({
     subValuesUnique: false,
   });
 
+  const selectFieldData = getFieldData({
+    fieldPath: `${fieldPath}.0.titleType`,
+    fieldRepresentation: "compact",
+  });
+
   return (
     <ArrayField
       addButtonLabel={addButtonLabel}
@@ -75,10 +80,7 @@ export const AdditionalTitlesField = ({
                 clearable
                 id={`${fieldPathPrefix}.titleType`}
                 width={16}
-                {...getFieldData({
-                  fieldPath: `${fieldPathPrefix}.titleType`,
-                  fieldRepresentation: "compact",
-                })}
+                {...selectFieldData}
               />
             </Form.Field>
           </ArrayFieldItem>
